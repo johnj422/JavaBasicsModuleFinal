540086a9d02a563322c6a24fd0372f260e48bc60
john.arango <johnj42@gmail.com>
2022-10-25 13:11:29 -0500

    University Class implemented

--- src/org/globantUniversity/data/University.java
+++ src/org/globantUniversity/data/University.java
@@ -1,4 +1,21 @@
 package org.globantUniversity.data;
 
+import java.util.ArrayList;
+
 public class University {
+    private ArrayList<Subject> subjectsList;
+    private ArrayList<Teacher> teachersList;
+    private ArrayList<Student> studentsList;
+
+    public ArrayList<Subject> getSubjectsList() {
+        return subjectsList;
+    }
+
+    public ArrayList<Teacher> getTeachersList() {
+        return teachersList;
+    }
+
+    public ArrayList<Student> getStudentsList() {
+        return studentsList;
+    }
 }
