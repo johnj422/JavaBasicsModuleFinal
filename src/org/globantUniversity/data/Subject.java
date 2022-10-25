2d965e89c6f4d8b83ffa3d763fb79a6856999a40
john.arango <johnj42@gmail.com>
2022-10-25 13:05:43 -0500

    Subject Class implemented

--- src/org/globantUniversity/data/Subject.java
+++ src/org/globantUniversity/data/Subject.java
@@ -1,4 +1,40 @@
 package org.globantUniversity.data;
 
+import java.util.ArrayList;
+
 public class Subject {
+    private String name;
+    private int classRoom;
+    private ArrayList<Student> studentsList;
+    private Teacher teacher;
+
+    public Subject(String name, int classRoom, Teacher teacher) {
+        this.name = name;
+        this.classRoom = classRoom;
+        this.teacher = teacher;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getClassRoom() {
+        return classRoom;
+    }
+
+    public void setClassRoom(int classRoom) {
+        this.classRoom = classRoom;
+    }
+
+    public Teacher getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
 }
