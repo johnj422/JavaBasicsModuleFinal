11813fa6730a9b76f1bf961247e0f94efb1edd5e
john.arango <johnj42@gmail.com>
2022-10-31 19:37:37 -0500

    JavaDoc generated for FullTimeTeacher class

--- src/org/globantUniversity/data/FullTimeTeacher.java
+++ src/org/globantUniversity/data/FullTimeTeacher.java
@@ -9,6 +9,10 @@ public class FullTimeTeacher extends Teacher {
         this.baseSalary = 5000;
     }
 
+    /**
+     * Calculates teacher's base salary based on its experienceYears.
+     * @return Teacher's base salary.
+     */
     @Override
     public double calculateSalary() {
         return baseSalary * 1.1 * experienceYears;
